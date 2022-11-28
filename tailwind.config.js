af5f6a59233e699a65868ef4b9ac64e97c2a8809
Maycon Coutinho <65024482+coutinho-maycon@users.noreply.github.com>
2022-11-27 23:11:33 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'cork': {          
+          '900': '#43291f',        
+        },
+        'sidecar': {          
+          '100': '#f4f0bb',          
+        },
+        'de-york': {          
+          '300': '#87c38f',          
+        },
+        'green-pea': {          
+          '600': '#226f54',          
+        },
+        'punch': {          
+          '600': '#da2c38',          
+      },
+      
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      
+      },
+    },
+  },
+  plugins: [],
+}
