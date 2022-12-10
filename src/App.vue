81943c9f6b11a1f26c9d094241842e5d47ff6ba1
Maycon Coutinho <65024482+coutinho-maycon@users.noreply.github.com>
2022-12-09 22:28:39 -0300

    Começo dos desafios da Codelândia

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>  
-  <TwitterClone></TwitterClone>
+  <desafioUm></desafioUm>
 </template>
 
 <script>
 //import HomePage from './components/HomePage.vue',
-import TwitterClone from './components/TwitterClone.vue'
+//import TwitterClone from './components/TwitterClone.vue',
+import desafioUm from './components/desafioUm.vue'
 import './assets/style.css'
 
 export default {
   name: 'App',
   components: {
     //HomePage,
-    TwitterClone,  
+    //TwitterClone,
+    desafioUm,
   }
 }
 </script>
---
+++ src/components/desafioUm.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="bg-gradient-to-r from-indigo-600 to-sky-400 flex flex-col pt-5">
+            <div class="flex text-white font-semibold container mx-auto px-4 pb-10">   
+                <p class="mx-auto">Codelândia</p>
+                <p class="mx-auto">Blog</p>
+            </div>
+            <div>
+                <label class="relative block w-1/2 mx-auto mb-5">                    
+                    <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+                        <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" stroke-width="1.5" stroke="currentColor" ><path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" /></svg>
+                    </span>
+                    <input class="placeholder:text-white block bg-slate-50/30 w-full rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Procurar no blog" type="text" name="search"/>
+                </label>
+                
+            </div>
+        </div>
+    </div>
+</template>
