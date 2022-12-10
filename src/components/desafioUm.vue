9d019010da0e993f4bc85b6b5b5497b5ee75ccbe
Maycon Coutinho <65024482+coutinho-maycon@users.noreply.github.com>
2022-12-09 23:22:42 -0300

    Termino do dessafio número 1 - Codelândia

--- src/components/desafioUm.vue
+++ src/components/desafioUm.vue
@@ -11,9 +11,43 @@
                         <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" stroke-width="1.5" stroke="currentColor" ><path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" /></svg>
                     </span>
                     <input class="placeholder:text-white block bg-slate-50/30 w-full rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Procurar no blog" type="text" name="search"/>
-                </label>
-                
+                </label>                
             </div>
         </div>
+    
+        <div class="bg-gray-100  pt-16">
+            <div class="bg-white container mx-auto w-1/2 card rounded p-4 mb-8">
+                <div class="flex justify-between">
+                    <p class="text-gray-700 text-xs">02 de jul, 2021</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hover:fill-red-400 hover:stroke-red-400 transition duration-700 ease-in-out"><path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" /></svg>
+                </div>
+                <p class="font-bold text-gray-700 mb-5">Agora é oficial: O windows 11 está vindo</p>
+                <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis.</p>
+            </div>
+            <div class="bg-white container mx-auto w-1/2 card rounded p-4 mb-8">
+                <div class="flex justify-between">
+                    <p class="text-gray-700 text-xs">02 de jul, 2021</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hover:fill-red-400 hover:stroke-red-400 transition duration-700 ease-in-out"><path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" /></svg>
+                </div>
+                <p class="font-bold text-gray-700 mb-5">Tim Berners-Lee vai leiloar código-fonte da web</p>
+                <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero.</p>
+            </div>
+            <div class="bg-white container mx-auto w-1/2 card rounded p-4 mb-8">
+                <div class="flex justify-between">
+                    <p class="text-gray-700 text-xs">02 de jul, 2021</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hover:fill-red-400 hover:stroke-red-400 transition duration-700 ease-in-out"><path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" /></svg>
+                </div>
+                <p class="font-bold text-gray-700 mb-5">Tem Firefox novo no pedaço e você vai querer migrar</p>
+                <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est.</p>
+            </div>
+            <div class="bg-white container mx-auto w-1/2 card rounded p-4">
+                <div class="flex justify-between">
+                    <p class="text-gray-700 text-xs">02 de jul, 2021</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hover:fill-red-400 hover:stroke-red-400 transition duration-700 ease-in-out"><path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" /></svg>
+                </div>
+                <p class="font-bold text-gray-700 mb-5">John McAfee, criador do antivírus McAfee, morre</p>
+                <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero.</p>
+            </div>
+        </div>        
     </div>
 </template>
