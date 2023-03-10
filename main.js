11dc0c0f0053a4641f9b613d04ca94582f674de6
Maycon Coutinho <65024482+coutinho-maycon@users.noreply.github.com>
2023-03-09 22:21:53 -0300

    fechando funcional

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const menu = document.getElementsByClassName('menu')
 
 function fecharMenu(){
-    menu[0].classList.add('sumir-menu')
-    
+  menu[0].classList.add('sumir-menu')
+  console.log("fechou")
 }
-
-console.log(menu)
