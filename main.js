3b417429b1eb344e891cb8ae78430b28b603315c
Maycon Coutinho <65024482+coutinho-maycon@users.noreply.github.com>
2023-03-09 22:47:59 -0300

    menu responsivo 100% finalizado

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ function fecharMenu(){
   menu[0].classList.add('sumir-menu')
   console.log("fechou")
 }
+
+function abrirMenu(){
+  menu[0].classList.remove('sumir-menu')
+  console.log("abriu")
+}
