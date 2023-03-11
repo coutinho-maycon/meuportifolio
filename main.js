bfa428835b4e677ebb0ce960f69281f85fdc11ce
Maycon Coutinho <65024482+coutinho-maycon@users.noreply.github.com>
2023-03-10 22:27:17 -0300

    Corrigido o bug do nav bar que ao recarregar já aparecia o menu

--- main.js
+++ main.js
@@ -11,13 +11,14 @@ document.addEventListener('DOMContentLoaded', () => {
     .go();
 })
 const menu = document.getElementsByClassName('menu')
-
-function fecharMenu() {
-  menu[0].classList.add('sumir-menu')
-  console.log("fechou")
-}
-
 function abrirMenu() {
-  menu[0].classList.remove('sumir-menu')
+  menu[0].classList.remove('menu_')
   console.log("abriu")
 }
+
+function fecharMenu() {
+  menu[0].classList.add('menu_')
+  console.log("fechou")
+
+}
+
